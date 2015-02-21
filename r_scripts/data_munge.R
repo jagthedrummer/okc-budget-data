26554102df744b86618d269fc8622aa63212e62b
Jeremy Green <jeremy@octolabs.com>
2015-02-21 14:34:10 -0600

    Added a few more munged data sets for evaluation

--- r_scripts/data_munge.R
+++ r_scripts/data_munge.R
@@ -40,7 +40,56 @@ write.csv(forbubble,"../data/generated/by_lob.csv",row.names=FALSE)
 
 
 
-forbubble <- okcbudget %>% group_by("Account Description") %>%
+forbubble <- okcbudget %>% group_by(Account.Description) %>%
         summarise(Total=sum(FY2015.Budget))
 
 write.csv(forbubble,"../data/generated/by_account.csv",row.names=FALSE)
+
+
+
+
+
+
+
+
+
+forbubble <- okcbudget %>% group_by(FundDescription,OperatingUnitDescription) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_fund_and_operating_unit.csv",row.names=FALSE)
+
+
+
+
+forbubble <- okcbudget %>% group_by(FundDescription,ProgramName) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_fund_and_program.csv",row.names=FALSE)
+
+
+
+forbubble <- okcbudget %>% group_by(FundDescription,LOBName) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_fund_and_lob.csv",row.names=FALSE)
+
+
+
+forbubble <- okcbudget %>% group_by(FundDescription,Account.Description) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_fund_and_account.csv",row.names=FALSE)
+
+
+
+
+
+
+
+
+
+fortree <- okcbudget %>% 
+        group_by(FundDescription,OperatingUnitDescription,ProgramName,LOBName,Account.Description) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(fortree,"../data/generated/tree.csv",row.names=FALSE)
