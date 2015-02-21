a36252c7b3989a000944822c1380f6f164e57c60
Jeremy Green <jeremy@octolabs.com>
2015-02-21 13:44:42 -0600

    Rough script for grouping the raw data is in place. Changes are sure to come soon.

--- r_scripts/data_munge.R
+++ r_scripts/data_munge.R
@@ -1,16 +1,46 @@
-#! Rscript
+#! /usr/local/bin/Rscript
 
 library(dplyr)
 library(jsonlite)
 
 okcbudget <- read.csv("../data/okc_fy2015_budget_detail.csv")
 
-fortree <- okcbudget %>% 
-        group_by(FundDescription,OperatingUnitDescription,ProgramName,LOBName,Account.Description) %>%
+
+
+forbubble <- okcbudget %>% group_by(FundDescription) %>%
         summarise(Total=sum(FY2015.Budget))
 
-forbubble <- okcbudget %>%
-        group_by(FundDescription) %>%
+write.csv(forbubble,"../data/generated/by_fund.csv",row.names=FALSE)
+
+
+
+
+forbubble <- okcbudget %>% group_by(OperatingUnitDescription) %>%
         summarise(Total=sum(FY2015.Budget))
 
-write.csv(forbubble,"forbubble.csv",row.names=FALSE)
+write.csv(forbubble,"../data/generated/by_operating_unit.csv",row.names=FALSE)
+
+
+
+
+forbubble <- okcbudget %>% group_by(ProgramName) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_program.csv",row.names=FALSE)
+
+
+
+
+
+forbubble <- okcbudget %>% group_by(LOBName) %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_lob.csv",row.names=FALSE)
+
+
+
+
+forbubble <- okcbudget %>% group_by("Account Description") %>%
+        summarise(Total=sum(FY2015.Budget))
+
+write.csv(forbubble,"../data/generated/by_account.csv",row.names=FALSE)
